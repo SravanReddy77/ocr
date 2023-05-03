1cad17d1eab53ee1a337bf7c74f68e441d939955
sravanreddy annadi <sravanreddy.annadi7777@gmail.com>
2023-05-03 08:32:36 -0400

    add test cases
    add test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Recognition from './Recognition';
+import PreprocessImage from './Preprocess';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('check image file type', () => {
+
+render(<Recognition />)
+
+})
+
+test ('check css', () => {
+
+  render(<App />)
+
+})
+
+test('check index', () => {
+
+  render(<index />)
+  
+})
+
